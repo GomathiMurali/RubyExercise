dadf2e2cac46d87e429af1f8f44050bf82308026
Gomathi <gomathi.787@gmail.com>
2020-03-03 12:03:31 +0530

    inital code commit

---
+++ Exercise/ex3.rb
@@ -0,0 +1,23 @@
+puts "I will now count my chickens:"
+
+puts "Hens #{30 + 30 / 6}"
+puts "Roosters #{100 - 30 * 3 % 4}"
+
+puts "Now I will count the eggs:"
+
+puts 5 + 2 * 1 - 6 % 2 - 5 / 4 + 5
+
+puts "Is it true that 3 + 2 < 5 - 6"
+
+puts 3 + 2 < 5 - 7
+
+puts "what is 3 + 2 ? #{3 + 2}"
+puts "what is 5 - 7? #{5 - 7}"
+
+puts "oh, that's why it's false"
+
+puts "How about some more"
+
+puts "Is it greater? #{5 > -2}"
+puts "Is it greater or equal? #{5 >= -2}"
+puts "Is it less or equal? #{5 <= -2}"
---
+++ Exercise/ex9.rb
@@ -0,0 +1,11 @@
+days = "Mon Tue Wed Thu Fri Sat Sun"
+month = "\nJan\nFeb\nMar\nApr\nMay\nJun\nJul\nAug\nsep\noct\nnov\nDec"
+
+puts "Here are the days: #{days}"
+puts "Here are the months: #{month}"
+
+puts %q{
+        There's something going on here.
+        With this weird quote
+        We'll be able to type as much as we like.
+        Even 4 lines if we want, or 5, or 6. }
